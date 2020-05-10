1b9b41525dd7d2d0a740e5d012048d3f47156b7e
Joey <joeystil3@gmail.com>
2020-05-10 14:44:44 +0200

    Version 1 template

---
+++ spa/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <h1>This is an react app with ts!</h1>
+        </div>
+    );
+};
+
+export default App;
